b1ac7098a33ad25b9eca40f0cb074fd08e2c2708
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-03-14 15:26:40 +0100

    Fix Karma Configuration

--- ui.frontend/karma.conf.js
+++ ui.frontend/karma.conf.js
@@ -44,7 +44,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'], // To be able to run tests in Docker
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }, // To be able to run tests in Docker
     // to avoid DISCONNECTED messages
     browserDisconnectTimeout : 10000, // default 2000
     browserDisconnectTolerance : 1, // default 0
